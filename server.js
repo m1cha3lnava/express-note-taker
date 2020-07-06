52d19134989597459cc5e00719399305568e439e
Michael Nava <M1cha3l.nava@gmail.com>
2020-07-06 17:35:15 -0400

    pushed from master by accident

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 app.get("/", (req,res) => {
     res.send("Hello World");
-} )
+})
 
 app.listen(PORT, (req, res) => {
     console.log(`Currently running on http://localhost:${PORT}`)
