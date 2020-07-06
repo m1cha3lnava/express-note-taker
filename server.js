5ebaef75fb4bfb702dcb7e5d96374fc874bc7735
Michael Nava <M1cha3l.nava@gmail.com>
2020-07-06 14:42:30 -0400

    receiving response from server

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.get("/", )
+app.get("/", (req,res) => {
+    res.send("Hello World");
+} )
 
 app.listen(PORT, (req, res) => {
     console.log(`Currently running on http://localhost:${PORT}`)
