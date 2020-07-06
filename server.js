80955043ccafc8be90f04c5a06838f3c4d0cb669
Michael Nava <M1cha3l.nava@gmail.com>
2020-07-06 17:54:40 -0400

    created all the directories

--- server.js
+++ server.js
@@ -2,14 +2,24 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({extended: true}));
+// app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "notes.html"));
+  });
+
+app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
   });
 
-
 app.listen(PORT, (req, res) => {
-    console.log(`Currently running on http://localhost:${PORT}`)
-})
+  console.log(`Currently running on http://localhost:${PORT}`);
+});
+
